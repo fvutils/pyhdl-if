0c26eac6ed32c76ad3fa16f0bf5c830380ea6caa
Matthew Ballance <matt.ballance@gmail.com>
2024-08-06 08:59:43 -0700

    Tweak CI to work with Node.js changes
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ scripts/build_linux.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -x
+
+echo "BUILD_NUM=${BUILD_NUM}" >> python/hdl_if/__build_num__.py
+${IVPM_PYTHON} -m pip install ivpm cython
+${IVPM_PYTHON} -m ivpm update -a
+
+PYTHON=./packages/python/bin/python
+${PYTHON} -m pip install twine auditwheel ninja wheel cython
+${PYTHON} setup.py bdist_wheel
+
+for whl in dist/*.whl; do
+    ${PYTHON} -m auditwheel repair $whl
+    rm $whl
+done
