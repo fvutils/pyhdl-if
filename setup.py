8a54510c92d06358735ef852cd89eb0935a0cb5e
Matthew Ballance <matt.ballance@gmail.com>
2025-10-25 00:42:11 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- setup.py
+++ setup.py
@@ -133,16 +133,16 @@ if isSrcBuild:
 
         print("<-- gen_py_api")
 
-        print("--> gen_via_if")
-        cmd = [sys.executable, 
-               os.path.join(proj_dir, "scripts", "gen_via_if.py")]
+        print("--> gen_uvm_if")
+#        cmd = [sys.executable, 
+#               os.path.join(proj_dir, "scripts", "gen_via_if.py")]
         
-        res = subprocess.run(cmd, stderr=subprocess.STDOUT)
+#        res = subprocess.run(cmd, stderr=subprocess.STDOUT)
 
-        if res.returncode != 0:
-            raise Exception("Failed to run cmd %s" % str(cmd))
+#        if res.returncode != 0:
+#            raise Exception("Failed to run cmd %s" % str(cmd))
 
-        print("<-- gen_via_if")
+        print("<-- gen_uvm_if")
 
     setup_args["ivpm_hooks"] = {
         "setup.pre" : [
