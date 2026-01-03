c1cc45d658a2a05ad6fb818cc2d8c0e641e67e36
Matthew Ballance <matt.ballance@gmail.com>
2026-01-03 01:35:42 +0000

    Fix version build script: use > instead of >> to overwrite __build_num__.py
    The build script was using >> (append) which could cause issues if the file
    already exists. Changed to > (overwrite) to ensure clean build number injection
    for each CI build.

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-echo "BUILD_NUM=${BUILD_NUM}" >> src/hdl_if/__build_num__.py
+echo "BUILD_NUM=${BUILD_NUM}" > src/hdl_if/__build_num__.py
 ${IVPM_PYTHON} -m pip install ivpm cython
 if test $? -ne 0; then exit 1; fi
 
